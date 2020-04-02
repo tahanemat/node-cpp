6ca6247f969c71a706df6b54697099a0ff8ccb87
Taha Nemat <tahanemat6@gmail.com>
2020-04-02 17:23:47 +0500

    created a basic addon with no output

---
+++ build/Release/.deps/Release/nodecpp.node.d
@@ -0,0 +1,1 @@
+cmd_Release/nodecpp.node := rm -rf "Release/nodecpp.node" && cp -af "Release/obj.target/nodecpp.node" "Release/nodecpp.node"
---
+++ index.js
@@ -0,0 +1,4 @@
+const nodecpp = require('./build/Release/nodecpp.node');
+console.log('-------------->');
+console.log(nodecpp);
+module.exports = nodecpp;
